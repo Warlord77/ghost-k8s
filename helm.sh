95b41719f5dea1eb5870fefb124d6c0ff54123f6
warlord77 <sahiljoseph20@gmail.com>
2021-05-06 04:16:28 +0530

    working ssl for ghost

---
+++ helm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install nginx-ingress ingress-nginx/ingress-nginx --set controller.publishService.enabled=true
+kubectl get svc nginx-ingress-ingress-nginx-controller
+kubectl get pods
+kubectl create namespace cert-manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.2.0 --set installCRDs=true
+kubectl get pods --namespace cert-manager
